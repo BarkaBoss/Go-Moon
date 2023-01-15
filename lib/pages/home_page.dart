fbca240548f0fb2865b9e329aef7000fbf1411fb
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-15 01:57:50 +0100

    Device Dimentions

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(child: _pageTitle(),)
+        body: SafeArea(
+      child: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.1),
+        child: _pageTitle(),
+      ),
+    ));
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
     );
   }
 
-  Widget _pageTitle(){
-    return const Text("#GoMoon", style: TextStyle(
-      color: Colors.white,
-      fontSize: 70,
-      fontWeight: FontWeight.w800
-    ),);
-  }
-
-  Widget _astroWidget(){
+  Widget _astroWidget() {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
